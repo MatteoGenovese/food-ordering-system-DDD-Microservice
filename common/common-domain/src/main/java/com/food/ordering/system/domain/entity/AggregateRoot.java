3e1ce34b622db8d801d703d1009962a7cc0190f5
MatteoGenovese <matteo.genovese@icloud.com>
2026-01-06 12:30:25 +0100

    added several value objects

--- common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
@@ -2,6 +2,4 @@ package com.food.ordering.system.domain.entity;
 
 //Marker class for distinguish aggregate roots object from other entities
 public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
-
-
 }
