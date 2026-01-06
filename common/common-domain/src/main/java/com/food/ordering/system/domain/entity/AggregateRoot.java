8a0c824dd14ed847ab6f027f8428f80214d7bb08
MatteoGenovese <matteo.genovese@icloud.com>
2026-01-06 11:54:30 +0100

    add common-domain

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
@@ -0,0 +1,7 @@
+package com.food.ordering.system.domain.entity;
+
+//Marker class for distinguish aggregate roots object from other entities
+public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
+
+
+}
