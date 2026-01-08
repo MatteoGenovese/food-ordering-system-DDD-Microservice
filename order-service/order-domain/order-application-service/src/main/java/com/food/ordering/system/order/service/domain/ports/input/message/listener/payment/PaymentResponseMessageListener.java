926942f6388115213d302e40feed4e42405ee973
MatteoGenovese <matteo.genovese@icloud.com>
2026-01-08 09:41:22 +0100

    Adding Mapper class and port definitions to Order Application Service

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/payment/PaymentResponseMessageListener.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.order.service.domain.ports.input.message.listener.payment;
+
+import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import jakarta.validation.Valid;
+
+public interface PaymentResponseMessageListener {
+
+    void paymentComplete(PaymentResponse paymentResponse);
+
+    void paymentCancelled(PaymentResponse paymentResponse);
+
+}
